80f78d118d049690fc7e000a5a38334220e5aad7
Anirudh Sugavi <38699686+anirudhsugavi@users.noreply.github.com>
2018-07-05 01:04:51 +0530

    Changed indentation to spaces from tabs

--- intro-alg/src/clrs/misc/BinaryAddition.java
+++ intro-alg/src/clrs/misc/BinaryAddition.java
@@ -4,27 +4,27 @@ import java.util.Arrays;
 
 public class BinaryAddition {
 
-	public static void main(String[] args) {
-		int[] a = {1,1,0,1,1};
-		int[] b = {1,0,1,1,1};
-		
-		int[] sum = add(a, b);
-		System.out.println(Arrays.toString(sum));
-	}
+    public static void main(String[] args) {
+        int[] a = { 1, 1, 0, 1, 1 };
+        int[] b = { 1, 0, 1, 1, 1 };
 
-	public static int[] add(int[]a, int[]b) {
-		int n = a.length;
-		int[] sum = new int[n + 1];
-		
-		int c = 0;
-		
-		for (int i = n - 1; i >= 0; i--) {
-			sum[i + 1] = a[i] ^ b[i] ^ c;
-			if ((c + a[i] + b[i]) > 1) {
-				c = 1;
-			}
-		}
-		sum[0] = c;
-		return sum;
-	}
+        int[] sum = add(a, b);
+        System.out.println(Arrays.toString(sum));
+    }
+
+    public static int[] add(int[] a, int[] b) {
+        int n = a.length;
+        int[] sum = new int[n + 1];
+
+        int c = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            sum[i + 1] = a[i] ^ b[i] ^ c;
+            if ((c + a[i] + b[i]) > 1) {
+                c = 1;
+            }
+        }
+        sum[0] = c;
+        return sum;
+    }
 }
